e1ecc42c073549c47a09206e9e811863597df80e
muhammad hamza <hamzaabdulmajeed863@gmail.com>
2024-11-29 17:23:42 +0500

    backend imp

--- restaurant-app-nextjs/src/app/signin/page.js
+++ restaurant-app-nextjs/src/app/signin/page.js
@@ -111,7 +111,7 @@ export default function Signin() {
 // }
 //   };
 try {
-  const response = await fetch("http://localhost:3002/users/getUser", {
+  const response = await fetch("https://restaurant-app-next-ruddy.vercel.app/users/getUser", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
